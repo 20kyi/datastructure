b0b3d66731b324559455b5884967707b65d1c44d
gurumee <gurumee92@gmail.com>
2019-09-13 13:35:58 +0900

    BubbleSort code complete

---
+++ src/ch13/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void BubbleSort(int arr[], int n) {
+    for (int i=0; i<n-1; i++) { 
+
+        for (int j=0; j< (n-1); j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+
+    int arr[] = { 8, 2, 3, 7, 1, 5, 4, 6, 9 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("정렬 전 : ");
+    for (int i=0; i<size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    BubbleSort(arr, size);
+
+    printf("정렬 후 : ");
+    for (int i=0; i<size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
