d9954fb5cd30500d3070ddc878250375e398c0da
gurumee <gurumee92@gmail.com>
2019-09-13 14:54:03 +0900

    Bubble Sort Description Complete

--- src/ch13/main.c
+++ src/ch13/main.c
@@ -3,7 +3,7 @@
 void BubbleSort(int arr[], int n) {
     for (int i=0; i<n-1; i++) { 
 
-        for (int j=0; j< (n-1); j++) {
+        for (int j=0; j< (n-1-i); j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -15,7 +15,7 @@ void BubbleSort(int arr[], int n) {
 
 int main() {
 
-    int arr[] = { 8, 2, 3, 7, 1, 5, 4, 6, 9 };
+    int arr[] = { 8, 2, 3, 6, 5, 7, 4, 1 };
     int size = sizeof(arr) / sizeof(arr[0]);
 
     printf("정렬 전 : ");
